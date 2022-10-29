356273220103459a10002d8f67282da0a2382575
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-10-29 15:17:58 +0530

    Interfaces seperated to seperate file

---
+++ src/state/actions/index.ts
@@ -0,0 +1,14 @@
+interface DepositAction {
+    type:"deposit"
+    payload: number
+}
+interface WithdrawAction{
+    type: "withdraw"
+    payload: number
+}
+interface BankruptAction{
+    type: "bankrupt"
+    
+}
+
+export type Action = DepositAction | WithdrawAction | BankruptAction
--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -1,22 +1,7 @@
 import { type } from "os";
+import { Action } from "../actions";
 
 const initialState = 0;
-
-interface DepositAction {
-    type:"deposit"
-    payload: number
-}
-interface WithdrawAction{
-    type: "withdraw"
-    payload: number
-}
-interface BankruptAction{
-    type: "bankrupt"
-    
-}
-
-type Action = DepositAction | WithdrawAction | BankruptAction
-
 const reducer = (state: number = initialState, action: Action) => {
     switch (action.type) {
         case "deposit":
